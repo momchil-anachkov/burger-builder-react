bc68ed319b16d93cdea89dab2ed27784f7a91915
Momchil Anachkov <momchil.anachkov@gmail.com>
2019-05-18 15:52:13 +0300

    add signup

--- src/store/actions/auth.ts
+++ src/store/actions/auth.ts
@@ -1,4 +1,6 @@
 import { AuthStart, ActionTypes, AuthSuccess, AuthFail } from './actionTypes';
+import axios from 'axios';
+import { API_KEY } from '../../keys/firebase';
 
 export const authStart = (): AuthStart  => ({
   type: ActionTypes.AUTH_START,
@@ -16,4 +18,23 @@ export const authFail = (error: Error): AuthFail  => ({
 
 export const auth = (email: string, password: string) => (dispatch: Function) => {
   dispatch(authStart());
+  const authData = {
+    email: email,
+    password: password,
+    returnSecureToken: true,
+  }
+
+  axios.post(
+    `https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=${API_KEY}`,
+    authData,
+  )
+    .then((response) => {
+      // dispatch
+      console.log(response);
+      dispatch(authSuccess(response.data))
+    })
+    .catch((error: Error) => {
+      console.log(error);
+      dispatch(authFail(error))
+    });
 }
