474159978898a304a607496ba3f0186d3e15dd0f
Momchil Anachkov <momchil.anachkov@gmail.com>
2019-06-04 16:34:28 +0300

    lazy-load auth, orders & checkout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import classes from './App.module.scss';
 import Layout from './hoc/Layout/Layout';
-import Checkout from './containers/Checkout/Checkout';
 import { Switch, Route, withRouter, Redirect } from 'react-router';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import { MapDispatchToProps, connect, MapStateToProps } from 'react-redux';
 import { AppDispatchProps, AppProps, AppOwnProps, AppStateProps } from './App.props';
 import { AuthOwnProps } from './containers/Auth/Auth.props';
 import { authInit } from './store/actions/auth';
 import { AppState } from './store/app.state';
+
+const Checkout = React.lazy(() => import('./containers/Checkout/Checkout'));
+const Orders = React.lazy(() => import('./containers/Orders/Orders'));
+const Auth = React.lazy(() => import('./containers/Auth/Auth'));
+
+// import Checkout from './containers/Checkout/Checkout';
+// import Orders from './containers/Orders/Orders';
+// import Auth from './containers/Auth/Auth';
 
 class App extends Component<AppProps> {
   state = {
@@ -30,23 +35,27 @@ class App extends Component<AppProps> {
     }
 
     let routes = (
-      <Switch>
-        <Route path="/auth" component={Auth} />,
-        <Route path="/" exact component={BurgerBuilder} />
-        <Redirect to="/"></Redirect>
-      </Switch>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/auth" component={Auth} />,
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      </Suspense>
     );
 
     if (this.props.isAuthenticated) {
       routes = (
-        <Switch>
-          <Route path="/checkout" component={Checkout} />,
-          <Route path="/orders" component={Orders} />,
-          <Route path="/logout" component={Logout} />,
-          <Route path="/auth" component={Auth} />,
-          <Route path="/" exact component={BurgerBuilder} />
-          <Redirect from="/auth" to="/"></Redirect>
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route path="/checkout" component={Checkout} />,
+            <Route path="/orders" component={Orders} />,
+            <Route path="/logout" component={Logout} />,
+            <Route path="/auth" component={Auth} />,
+            <Route path="/" exact component={BurgerBuilder} />
+            <Redirect from="/auth" to="/" />
+          </Switch>
+        </Suspense>
       );
     }
 
