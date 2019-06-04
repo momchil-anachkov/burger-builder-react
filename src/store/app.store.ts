a92acce88d86eab79cbe3e77126de0a4f7b19909
Momchil Anachkov <momchil.anachkov@gmail.com>
2019-06-04 16:20:17 +0300

    use redux devtools only in dev environment

--- src/store/app.store.ts
+++ src/store/app.store.ts
@@ -4,7 +4,7 @@ import burgerBuilderReducer from './reducers/burgerBuilder';
 import orderReducer from './reducers/order';
 import authReducer from './reducers/auth';
 
-const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 export const store = createStore(
   combineReducers({
