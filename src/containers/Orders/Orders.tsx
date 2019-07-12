aab2c766fe9f6edffdd1292dd70bac94d1d64953
Momchil Anachkov <momchil.anachkov@gmail.com>
2019-07-12 08:23:31 +0300

    switch orders to using hooks

--- src/containers/Orders/Orders.tsx
+++ src/containers/Orders/Orders.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Order from '../../components/Order/Order';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import { AppState } from '../../store/app.state';
@@ -8,34 +8,32 @@ import { fetchOrders } from '../../store/actions';
 import Spinner from '../../components/Spinner/Spinner';
 import axiosInstance from '../../axios';
 
-class Orders extends React.Component<OrdersProps> {
-  componentDidMount() {
-    this.props.fetchOrders(this.props.userId);
-  }
+const Orders = (props: OrdersProps) => {
+  useEffect(() => {
+    props.fetchOrders(props.userId);
+  }, []);
 
-  render = () => {
-    let ordersComponent: any = <Spinner />;
-    if (!this.props.loading) {
-      ordersComponent = this.props.orders.map((order: any) => (
-        <Order
-          key={ order.id }
-          ingredients={ order.ingredients }
-          price={order.price}
-        />)
-      );
-    }
-    return (
-      <div>
-        {ordersComponent}
-      </div>
+  let ordersComponent: any = <Spinner />;
+  if (!props.loading) {
+    ordersComponent = props.orders.map((order: any) => (
+      <Order
+        key={ order.id }
+        ingredients={ order.ingredients }
+        price={ order.price }
+      />)
     );
   }
+  return (
+    <div>
+      { ordersComponent }
+    </div>
+  );
 }
 
 const mapStateToProps: MapStateToProps<OrdersStateProps, OrdersOwnProps, AppState> = (state: AppState): OrdersStateProps => ({
-    loading: state.order.loading,
-    orders: state.order.orders,
-    userId: state.auth.userId!,
+  loading: state.order.loading,
+  orders: state.order.orders,
+  userId: state.auth.userId!,
 })
 
 const mapDispatchToProps: MapDispatchToPropsFunction<OrdersDispatchProps, OrdersOwnProps> = (dispatch: Function) => ({
